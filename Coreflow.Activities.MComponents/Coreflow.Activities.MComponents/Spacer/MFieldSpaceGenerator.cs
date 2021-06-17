db34a562c439dbfa1df5a17f562c67b7c403a20f
manureini <1554111+manureini@users.noreply.github.com>
2021-06-17 15:17:03 +0200

    added spacer

---
+++ Coreflow.Activities.MComponents/Coreflow.Activities.MComponents/Spacer/MFieldSpaceGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Markdig;
+using MComponents.MForm;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coreflow.Activities.MComponents
+{
+    public class MFieldSpaceGenerator : MFieldGenerator
+    {
+        public int Height { get; set; }
+
+        public override RenderFragment<MFieldGeneratorContext> Template => (context) => (builder) =>
+        {
+            builder.AddMarkupContent(0, $"<div class=\"m-field-space\" style=\"height: {Height}px\"></div>");
+        };
+    }
+}
---
+++ Coreflow.Activities.MComponents/Coreflow.Activities.MComponents/Spacer/MFormFieldSpaceActivity.cs
@@ -0,0 +1,45 @@
+﻿using Coreflow.Interfaces;
+using Coreflow.Objects;
+using MComponents.MForm;
+using MComponents.Shared.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coreflow.Activities.MComponents.Spacer
+{
+    [DisplayMeta("MField Space", "MForm", "fa-keyboard")]
+    public class MFormFieldSpaceActivity : ICodeActivity
+    {
+#pragma warning disable BL0005 // Component parameter should not be set outside of its component.
+
+        public void Execute(
+           [DisplayMeta("Fields")]
+           [DefaultValue("Fields")]
+           IList<IMField> pMFields,
+
+           [DisplayMeta("Row")]
+           [DefaultValue("mFormRow")]
+           int pRow,
+
+           [DisplayMeta("Height")]
+           int pHeight
+          )
+        {
+            var field = new MFieldSpaceGenerator()
+            {
+                Height = pHeight,
+                Attributes = new Attribute[] {
+                    new RowAttribute(pRow)
+                }
+            };
+
+            pMFields.Add(field);
+        }
+
+#pragma warning restore BL0005 // Component parameter should not be set outside of its component.
+
+    }
+}
