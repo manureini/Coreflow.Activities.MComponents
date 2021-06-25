9e1533f04dbb15a6dc9ad68915bd21d44a2335b9
manureini <1554111+manureini@users.noreply.github.com>
2021-06-25 18:04:31 +0200

    wip markdown editor

---
+++ Coreflow.Activities.MComponents/Coreflow.Activities.MComponents/Markdown/Markdown.cs
@@ -0,0 +1,31 @@
+﻿using Coreflow.Activities.MComponents;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+
+namespace Coreflow.Activities.MComponents.Markdown
+{
+    public class Markdown : ComponentBase
+    {
+        [Parameter]
+        public string Text { get; set; }
+
+        protected MarkupString mHtml;
+
+        protected static MarkdownPipeline Pipeline = MarkdownMFieldGenerator.Pipeline;
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.AddContent(0, mHtml);
+        }
+
+        protected override void OnParametersSet()
+        {
+            if (Text == null)
+                return;
+
+            mHtml = new MarkupString(Markdig.Markdown.ToHtml(Text, Pipeline));
+        }
+    }
+}
